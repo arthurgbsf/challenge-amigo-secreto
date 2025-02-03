ef737873fd30384c80840ddf4a61b45ff0fd7117
Arthur G Barbosa <arthurgbsf@gmail.com>
2025-02-02 21:44:50 -0300

    Implementação da função 'SortFriend'.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 function addFriend(){
+
     let friendName = document.getElementById("amigo");
     friendList.push(friendName.value);
     setTimeout( () => friendName.value = "", 400);  
 }
+
+function sortFriend(){
+
+    let [sortedFriend] = friendList.splice(parseInt(Math.random() * friendList.length), 1);
+    console.log(sortedFriend);
+}
+
+
 
 let friendList = [];
 
