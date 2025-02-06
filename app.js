51c94a360184c42f70c69b4b05ccc973d18ac05f
Arthur G Barbosa <arthurgbsf@gmail.com>
2025-02-05 22:12:54 -0300

    Implementação de validações e alguns ajustes.

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 function addFriend(){
-
-    let friendName = document.getElementById("amigo");
+    if(friendList.length == 0){
+        displayList.innerHTML = "";
+        secretFriend.innerHTML = "";
+    }
+    if(sortedFriendList != 0){
+        friendName.value = "";
+        return alert("Sorteio em andamento. Aguarde o próximo!");
+    }
+    if(friendName.value == ""){
+        return alert("Entre com o nome de uma pessoa.");
+    }
     friendList.push(friendName.value);
+    alert(`"${friendName.value}" foi adicionado com sucesso!`);
     setTimeout( () => friendName.value = "", 400);  
-}
-
+};
 function sortFriend(){
-
-    updateFriendList();
+    if (friendList.length == 0){
+        return alert("Nenhum amigo adicionado.");
+    };
+    if(sortedFriendList.length == 0){
+        if(!confirm("Tem certeza que deseja iniciar o sorteio?")){
+            return;
+        };
+    };
+    displaySortedFriendList();
     [sortedFriend] = friendList.splice(parseInt(Math.random() * friendList.length), 1);
     displaySecretFriend();
     sortedFriendList.push(sortedFriend);
-    
-}
-
-function updateFriendList(){
-
-    let displayList  = document.getElementById("listaAmigos");
+    if (friendList.length == 0){
+        sortedFriendList = [];
+        return alert("Sorteio finalizado.");
+    };
+};
+function displaySortedFriendList(){
     displayList.innerHTML = "";
     for( item of sortedFriendList){
         displayList.innerHTML += `<li> ${item} </li>`
-    }
-
-}
-
+    };
+};
 function displaySecretFriend(){
-    let secretFriend = document.getElementById("resultado");
     secretFriend.innerHTML = `<li> ${sortedFriend} </li>`;
-}
-
+};
 let sortedFriend = "";
 let friendList = [];
 let sortedFriendList = [];
+let friendName = document.getElementById("amigo");
+let displayList  = document.getElementById("listaAmigos");
+let secretFriend = document.getElementById("resultado");
